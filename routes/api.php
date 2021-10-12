20e2c01a6bd215380e294232898e91ad6dec1669
梶田英完 <hidemasa_kajita@respawn.jp>
2021-10-12 20:51:22 +0900

    add sanctum sample  api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/login', function () {
+    $user = Auth::loginUsingId(2);
+    $token = $user->createToken('test');
+    return response()->json($token);
 });
